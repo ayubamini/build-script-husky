9322a4d34c6ef9835acc69a72e3261d5b18ab988
ayyubamini <ayyub.amini@gmail.com>
2024-10-14 18:40:26 -0400

    First commit

---
+++ src/test-resolved.ts
@@ -0,0 +1,5 @@
+const foo = 'bar';
+
+function greet(name: string) {
+  console.log(`Hello, ${name}!`);
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+// This line will trigger a linting error
+const foo: any = 'bar';
+
+// This line will trigger a formatting error
+function greet(name:string){console.log(`Hello, ${name}!`)}
